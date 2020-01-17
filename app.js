00331db4cf3f603156b4e9907139910c3ca2e3e8
chungontsui <chungont@gmail.com>
2020-01-17 23:31:40 +1300

    Added Layout Design

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 Vue.component('todo-item', {
-    props: ['id'],
-    template: '<li>{{id}}</li>'
+    props: ['leg'],
+    template: "<form><input type='text' v-model='leg.startingPoint'/></form>"
 });
 
 
@@ -8,17 +8,17 @@ Vue.component('todo-item', {
 var app = new Vue({
     el: '#app',
     data: {
-      message: 'Hello Vue!',
+      message: '',
       tripLegs:[
-          {id: 1, startingPoint: "Christchurch", destination: "Dunedin", timeToGetThereInMin: 300, beginTravel: '0900', stayDurationAtDestInMin:}, //Till Midnight                                                                                     
-          {id: 2, startingPoint: "Dunedin", destination: "Queenstown", timeToGetThereInMin: 180, beginTravel: 'MORNING'}, 
-          {id: 3, startingPoint: "Queenstown", destination: "Christchurch", timeToGetThereInMin: 300, beginTravel: 'MORNING'}, 
-      2, 
-      3]
+          {id: 1, startingPoint: "Christchurch", destination: "Dunedin", timeToGetThereInMin: 300, beginTravel: '0900', stayDurationAtDestInMin: 720}, //Till Midnight                                                                                     
+          {id: 2, startingPoint: "Dunedin", destination: "Queenstown", timeToGetThereInMin: 180, beginTravel: '0900', stayDurationAtDestInMin: 720}, 
+          {id: 3, startingPoint: "Queenstown", destination: "Christchurch", timeToGetThereInMin: 300, beginTravel: '0900', stayDurationAtDestInMin: 720}, 
+      ]
     },
     methods: {
         addLeg: function(){
             this.tripLegs.push(this.tripLegs.length + 1);
+            this.message = this.tripLegs[(this.tripLegs.length - 1)].startingPoint;
         }
     }
 });
