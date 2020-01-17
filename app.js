096574c67a619ceb0a2e01492a8c3be72d518699
chungontsui <chungont@gmail.com>
2020-01-18 08:05:50 +1300

    Saving Progress

--- app.js
+++ app.js
@@ -1,19 +1,22 @@
 Vue.component('todo-item', {
     props: ['leg'],
-    template: "<form><input type='text' v-model='leg.startingPoint'/></form>"
+    template: `
+    <div class="input-group mb-3">
+    <input v-model='leg.startingPoint' type="text" class="form-control" placeholder="Username" aria-label="Username" aria-describedby="basic-addon1">
+    </div>
+    `
 });
-
 
 
 var app = new Vue({
     el: '#app',
     data: {
-      message: '',
-      tripLegs:[
-          {id: 1, startingPoint: "Christchurch", destination: "Dunedin", timeToGetThereInMin: 300, beginTravel: '0900', stayDurationAtDestInMin: 720}, //Till Midnight                                                                                     
-          {id: 2, startingPoint: "Dunedin", destination: "Queenstown", timeToGetThereInMin: 180, beginTravel: '0900', stayDurationAtDestInMin: 720}, 
-          {id: 3, startingPoint: "Queenstown", destination: "Christchurch", timeToGetThereInMin: 300, beginTravel: '0900', stayDurationAtDestInMin: 720}, 
-      ]
+        message: '',
+        tripLegs:[
+            {id: 1, startingPoint: "Christchurch", destination: "Dunedin", timeToGetThereInMin: 300, beginTravel: '0900', stayDurationAtDestInMin: 720}, //Till Midnight                                                                                     
+            {id: 2, startingPoint: "Dunedin", destination: "Queenstown", timeToGetThereInMin: 180, beginTravel: '0900', stayDurationAtDestInMin: 720}, 
+            {id: 3, startingPoint: "Queenstown", destination: "Christchurch", timeToGetThereInMin: 300, beginTravel: '0900', stayDurationAtDestInMin: 720}, 
+        ]
     },
     methods: {
         addLeg: function(){
